a533ff2811c442e9422231cafc25ce991cf49de8
walonCode <mohamedlaminwalonjalloh@gmail.com>
2026-01-31 00:38:21 +0000

    finish the struct section and understand struct in rust and impl

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -1,29 +1,76 @@
+
 struct User {
     active: bool,
     username: String,
     age:i32
 }
 
+//tuple struct
+struct Color(i32);
+
+// unit struct 
+struct AlwaysEqual;
+
+#[derive(Debug)]
+struct Rectangle {
+    width:u32,
+    height:u32
+}
+
+impl Rectangle{
+    fn area(&self) -> u32{
+        self.height * self.width
+    }
+    
+    fn square(size:u32) -> Self {
+        Self{
+            width:size,
+            height:size
+        }
+    }
+}
+
 fn main(){
     let mut  user1 = User{
         active: true,
-        username: String::from("walondev"),
+        username: String::from("Walon"),
         age:40,
     };
     
     user1.active = false;
     
     println!("{}, {}, {}",user1.active, user1.age, user1.username);
+
     
-    let user2 = build_user(String::from("walon"), 20);
     let user3 = User {
         active:false,
-        ..user2
+        ..user1
     };
     
     println!("{}", user3.username);
+    
+    let black = Color(0);
+    
+    println!("{}", black.0);
+    
+    let sq1 = Rectangle::square(32);
+    println!("the square is {sq1:#?}");
+    
+    let _subject = AlwaysEqual;
+    
+    let rect1 = Rectangle{
+        width:40,
+        height:30
+    };
+    
+    println!(
+        "The area of the rectangle is {} square pixel",
+        rect1.area()
+    );
+    
+   println!("rect1 is {rect1:#?}")
 }
 
-fn build_user(username:String,age:i32) -> User {
-    User { active: true, username, age }
-}
+// fn area(dimension: &Rectangle) -> u32 {
+//     dimension.width * dimension.height
+// }
